ffa6e8b0a2df431d74cbd14b20fead1f0481742e
Oleg <manunited150891@gmail.com>
2018-11-14 17:19:38 +0200

    Form validation with Formik and reactstrapp

---
+++ src/Form/Form.js
@@ -0,0 +1,171 @@
+import React, { Component, Fragment } from 'react';
+import { withFormik } from 'formik';
+import * as Yup from 'yup';
+import classnames from 'classnames';
+import { Button, Form, FormGroup, Label, Input, FormText, Row, Container, Col, FormFeedback } from 'reactstrap';
+
+class FormComponent extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <MyEnhancedForm user={{ email: '', firstName: '', lastName: '', password: '' }} />
+            </Fragment>
+        )
+    }
+}
+
+export default FormComponent
+
+const formikEnhancer = withFormik({
+    validationSchema: Yup.object().shape({
+        firstName: Yup.string()
+            .min(2, "Name must be longer than 2")
+            .max(12, "Name must be shorter than 12")
+            .required('First name is required.'),
+        lastName: Yup.string()
+            .min(2, "Name must be longer than 2")
+            .required('Last name is required.'),
+        email: Yup.string()
+            .email('Invalid email address')
+            .required('Email is required!'),
+        password: Yup.string()
+            .min(6, "Password bust be longer than 6")
+            .required("Please, enter your password")
+    }),
+
+    mapPropsToValues: ({ user }) => ({
+        ...user,
+    }),
+    handleSubmit: (payload, { setSubmitting }) => {
+        console.log(payload);
+        setSubmitting(false);
+    },
+    displayName: 'MyCustomForm',
+});
+
+
+
+const MyForm = props => {
+    const {
+        values,
+        touched,
+        errors,
+        dirty,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset,
+        isSubmitting,
+    } = props;
+    return (
+        // <Container>
+        //     <Row>
+        //         <Col xs="12">
+        <Form onSubmit={handleSubmit} >
+            <FormGroup>
+                <Label size='sm' mr="auto" className="text-info" for="firstName">First Name</Label>
+                <Input
+                    id="firstName"
+                    type="text"
+                    placeholder="John"
+                    error={touched.firstName && errors.firstName}
+                    value={values.firstName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    invalid={errors.firstName && touched.firstName}
+                    valid={!errors.firstName && touched.firstName}
+                />
+                <div className='feedbackWrapper'>
+                    <FormFeedback valid size="sm" >Looks good!</FormFeedback>
+                    <FormFeedback size="sm" className='text-danger'>{errors.firstName}</FormFeedback>
+                </div>
+                {/* {!errors.firstName && touched.firstName && <FormFeedback valid size="sm" >Looks good!</FormFeedback>} */}
+                {/* {errors.firstName && touched.firstName && <FormFeedback size="sm" className='text-danger'>{errors.firstName}</FormFeedback>} */}
+            </FormGroup>
+            <FormGroup>
+                <Label size="sm" className="text-info" for="lastName">Last Name</Label>
+                <Input
+                    id="lastName"
+                    type="text"
+                    placeholder="Doe"
+                    error={touched.lastName && errors.lastName}
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    invalid={errors.lastName && touched.lastName}
+                    valid={!errors.lastName && touched.lastName}
+                />
+                <div className='feedbackWrapper'>
+                    <FormFeedback valid size="sm" >Looks good!</FormFeedback>
+                    <FormFeedback size="sm" className='text-danger'>{errors.lastName}</FormFeedback>
+                </div>
+                {/* {errors.lastName && touched.lastName && <FormFeedback size="sm" className='text-danger'>{errors.lastName}</FormFeedback>}
+                {!errors.lastName && touched.lastName && <FormFeedback valid size="sm" >Looks good!</FormFeedback>} */}
+            </FormGroup>
+            <FormGroup>
+                <Label size="sm" className="text-info" for="email">Email</Label>
+                <Input
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    error={touched.email && errors.email}
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    invalid={errors.email && touched.email ? true : false}
+                    valid={!errors.email && touched.email}
+                />
+                 <div className='feedbackWrapper'>
+                    <FormFeedback valid size="sm" >Looks good!</FormFeedback>
+                    <FormFeedback size="sm" className='text-danger'>{errors.email}</FormFeedback>
+                </div>
+                {/* {!errors.email && touched.email && <FormFeedback valid size="sm" >Looks good!</FormFeedback>}
+                {errors.email && touched.email && <FormFeedback size="sm" className='text-danger'>{errors.email}</FormFeedback>} */}
+            </FormGroup>
+            <FormGroup>
+                <Label size="sm" className="text-info" for="password">Password</Label>
+                <Input
+                    id="password"
+                    type="password"
+                    placeholder="Enter your password"
+                    error={touched.password && errors.password}
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    invalid={errors.password && touched.password}
+                    valid={!errors.password && touched.password}
+                />
+                 <div className='feedbackWrapper'>
+                    <FormFeedback valid size="sm" >Looks good!</FormFeedback>
+                    <FormFeedback size="sm" className='text-danger'>{errors.password}</FormFeedback>
+                </div>
+                {/* {!errors.password && touched.password && <FormFeedback valid size="sm" >Looks good!</FormFeedback>}
+                {errors.password && touched.password && <FormFeedback size="sm" className='text-danger'>{errors.password}</FormFeedback>} */}
+            </FormGroup>
+            <FormGroup>
+                <Button
+                    color='secondary'
+                    type="button"
+                    className="outline"
+                    onClick={handleReset}
+                    disabled={!dirty || isSubmitting}
+                >
+                    Reset
+                            </Button>
+                <Button color="success" type="submit" disabled={isSubmitting}>
+                    Submit
+                            </Button>
+            </FormGroup>
+        </Form>
+        //         </Col>
+        //     </Row>
+        // </Container>
+
+    );
+};
+
+const MyEnhancedForm = formikEnhancer(MyForm);
